01ad43b5aa05fd62c29e018595affe9b518005fb
Michael <mikahado@gmail.com>
2022-10-04 00:34:16 -0400

    updates js, with option click listener for future versions

--- javascripts/index.js
+++ javascripts/index.js
@@ -2,10 +2,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let form = document.querySelector('form') //seeks 'form' class in html
     let msg = document.getElementById('comments')
+    // let specs = document.getElementById('myBtn')
 
     form.addEventListener('submit', (e) => { //readies the submit button, preps event(e) for function 
         e.preventDefault(); //safeguard for something
-        buildUserInput(e.target.todayDate.value, e.target.birthDate.value) //function below. 
+        monthCalc(e.target.todayDate.value, e.target.birthDate.value) //function below. 
  
     })
 
@@ -14,26 +15,24 @@ document.addEventListener("DOMContentLoaded", () => {
         myMsg(e.target.monthPlan.value) //function below. 
         msg.reset()
     })
+
+    // specs.addEventListener('click', (e) => { //readies the click button, preps event(e) for function 
+    //     e.preventDefault(); //safeguard for something
+    //     myFunction(e.target.specData.value); //function below. 
+    // })
+
 })
 
-function buildUserInput(todayDate, birthDate){
-    //caculates your age in months, posts it on the DOM
+function monthCalc(todayDate, birthDate){
+
     const todayYear = todayDate.slice(0, 4)
     const todayMonth = todayDate.slice(5, 7)
     const birthYear = birthDate.slice(0, 4)
     const birthMonth = birthDate.slice(5,7)
-    // console.log("today year:", todayYear)
-    // console.log("today month:", todayMonth)
-    // console.log("birth year:", birthYear)
-    // console.log("birth month:", birthMonth)
 
     monthsByYear = (todayYear - birthYear) * 12
-    monthsByYearAdjust = monthsByYear - birthMonth + 1
+    monthsByYearAdjust = monthsByYear - birthMonth
     monthAge = monthsByYearAdjust + parseInt(todayMonth)
-    // console.log("months by year:", monthsByYear)
-    // console.log("adjust for unlived birth-months:", monthsByYearAdjust)
-    // console.log("correct age in months", monthAge)
- 
     percentage = monthAge / 10
    
     let p = document.createElement('p')
@@ -45,17 +44,24 @@ function buildUserInput(todayDate, birthDate){
  async function fetchData() {
     const response = await fetch('/db.json');
     const lifeExpt = await response.json();
-    console.log(lifeExpt);
+    return lifeExpt
   }
 
-fetchData().then(lifeExpt => {
-    const exptArray = Object.values(lifeExpt)
-    const state = exptArray[0]
-    const total = exptArray[1]
-    
+  fetchData().then(lifeExpt => {
+    const data = lifeExpt.data.splice(0,51)
 
+    data.map(datum => {
+        const averageMonths = Math.ceil(datum[10] * 12) 
+        const state = datum[8]
+
+        const ul = document.querySelector('#specDatum')
+        const li = document.createElement('li')
+        li.textContent = state + ":   " + averageMonths + " months"
+        ul.append(li)
+    })
 })
 
+//functions for activities and montly plan
 
  getActivity()
  activity()
@@ -70,15 +76,13 @@ fetchData().then(lifeExpt => {
     const actArray = Object.values(acts);
     acts = actArray[0]
     let p = document.createElement('p')
-    p.textContent = "Dare to do something different. How about . . . " + acts + '!' 
+    p.textContent = "Dare to do something different. How about this: " + acts + '!' 
     document.querySelector('#activity').appendChild(p)
     }
 
  function myMsg(message){
-     //monthly plan textbox
     message = monthPlan.value
     let p = document.createElement('li')
     p.textContent = message 
     document.querySelector('#plans').appendChild(p)
  }
-
