d03376efb9dd2078b27fdc6c7c73c43f495c437e
Michael <mikahado@gmail.com>
2022-10-12 23:29:08 -0400

    recent changes, committed to main branch

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,102 +1,116 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-let form = document.querySelector('form') 
-let msg = document.getElementById('comments')
-let darkMode = localStorage.getItem('darkMode')
-
-form.addEventListener('submit', (e) => { 
-e.preventDefault(); 
-monthCalc(e.target.todayDate.value, e.target.birthDate.value) 
-})
-
-msg.addEventListener('submit', (e) => { 
-e.preventDefault(); 
-myMsg(e.target.monthPlan.value) 
-msg.reset()
-    })
-
-
-const darkModeToggle = document.querySelector('#dark-mode-toggle');
-
-const enableDarkMode = () => {
-    document.body.classList.add('darkmode')
-     localStorage.setItem('darkMode', 'enabled')
-   }
-
-const disableDarkMode = () => {
-       document.body.classList.remove('darkmode')
-       localStorage.setItem('darkMode', 'null')
-   }
-
-darkModeToggle.addEventListener('click', () => { 
-   darkMode = localStorage.getItem('darkMode') 
-   if (darkMode !== "enabled") {
-       enableDarkMode();
-    } else {
-       disableDarkMode();
-   }
-})
-
-})
-   
-getActivity()
-fetchLifeExpt()
-async function fetchLifeExpt() {
-            fetch('/db.json')
-            .then(response => response.json())
-            .then(data2 => lifeExpt(data2))
-          }
-    
-    function lifeExpt(entries) {
-        entries.splice(0,51).map(datum => {
-                
-            const averageMonths = Math.ceil(datum[10] * 12) 
-            const state = datum[8]
-            const fakeObject = state + ": " + averageMonths + " months."
-     
-            const ul = document.querySelector('#specDatum')
-            const li = document.createElement('li')
-            li.textContent = fakeObject
-            ul.append(li)
-                }     
-            )}
-
-    
-function getActivity(){
+    //GRAB INFO FROM ACTIVITY API
     fetch('http://www.boredapi.com/api/activity/')
     .then(response => response.json())
-    .then(data => activity(data))
- }
-
-function activity(act){
-    const actArray = Object.values(act);
-    act = actArray[0]
-    let p = document.createElement('p')
-    p.textContent = "Dare to do something different. How about this: " + act + '!' 
-    document.querySelector('#activity').appendChild(p)
+    .then(data1 => activity(data1))
+     
+    
+    //GRAB INFO FROM CDC DATABASE
+    fetch('/db.json')
+    .then(response => response.json())
+    .then(data2 => lifeExpt(data2))
+    
+    }) 
+    //END OF DOM 
+    
+    
+    //LIFE EXP FROM API NEEDS WORK!!
+    
+    function lifeExpt(entries) {
+        let averageMonths = 0
+        let state = ''
+    
+        for (item in entries){
+            console.log(item)
+            Math.ceil(item[10] * 12) += averageMonths 
+    
+            item[8] += state
+        }
+            const ul = document.querySelector('#specDatum')
+            const li = document.createElement('li')
+            li.textContent = state + ": " + averageMonths + " months."
+            ul.append(li)
+        }       
+    
+                  
+            
+    
+    //AGE-IN-MONTHS FEATURE
+     let form = document.querySelector('form') 
+    
+     form.addEventListener('submit', (e) => { 
+         e.preventDefault(); 
+         monthCalc(e.target.todayDate.value, e.target.birthDate.value) 
+         })
+    
+    function monthCalc(todayDate, birthDate){
+       
+        const todayYear = todayDate.slice(0, 4)
+        const todayMonth = todayDate.slice(5, 7)
+        const birthYear = birthDate.slice(0, 4)
+        const birthMonth = birthDate.slice(5,7)
+    
+        let monthsByYear = (todayYear - birthYear) * 12
+        let monthsByYearAdjust = monthsByYear - birthMonth
+        let monthAge = monthsByYearAdjust + parseInt(todayMonth)
+    
+        let percentage = monthAge / 10
+    
+        let p = document.createElement('p')
+        p.textContent = `You have lived  ${monthAge} months. That is ${percentage}% of your 1000 months here on Earth.` 
+        document.querySelector('#myInfo').appendChild(p)
+        
     }
-
-function monthCalc(todayDate, birthDate){
-
-    const todayYear = todayDate.slice(0, 4)
-    const todayMonth = todayDate.slice(5, 7)
-    const birthYear = birthDate.slice(0, 4)
-    const birthMonth = birthDate.slice(5,7)
-
-    let monthsByYear = (todayYear - birthYear) * 12
-    let monthsByYearAdjust = monthsByYear - birthMonth
-    let monthAge = monthsByYearAdjust + parseInt(todayMonth)
-
-    let percentage = monthAge / 10
-
-    let p = document.createElement('p')
-    p.textContent = `You have lived  ${monthAge} months. That is ${percentage}% of your 1000 months here on Earth.` 
-    document.querySelector('#myInfo').appendChild(p)
-}
-
-function myMsg(message){
-    const message = monthPlan.value
-    let p = document.createElement('li')
-    p.textContent = message 
-    document.querySelector('#plans').appendChild(p)
- }
+    
+    //GET IT DONE FEATURE
+    
+    let msg = document.getElementById('comments')
+    
+    msg.addEventListener('submit', (e) => { 
+        e.preventDefault(); 
+        getItDone(e.target.monthPlan.value) 
+        msg.reset()
+            })
+    
+    function getItDone(message){
+        message = monthPlan.value
+        let p = document.createElement('li')
+        p.textContent = message 
+        document.querySelector('#plans').appendChild(p)
+     }
+    
+     //SHAKE IT UP FEATURE
+    
+     function activity(act){
+        const actArray = Object.values(act);
+        act = actArray[0]
+    
+        let p = document.createElement('p')
+        p.textContent = "Dare to do something different. How about this: " + act + '!' 
+        document.querySelector('#activity').appendChild(p)
+        }
+    
+    //CSS MODE CHANGE
+    
+    let darkMode = localStorage.getItem('darkMode')
+    let darkModeToggle = document.querySelector('#dark-mode-toggle');
+    
+    const enableDarkMode = () => {
+        document.body.classList.add('darkmode')
+         localStorage.setItem('darkMode', 'enabled')
+       }
+    
+    const disableDarkMode = () => {
+           document.body.classList.remove('darkmode')
+           localStorage.setItem('darkMode', 'null')
+       }
+    
+    darkModeToggle.addEventListener('click', () => { 
+       darkMode = localStorage.getItem('darkMode') 
+       if (darkMode !== "enabled") {
+           enableDarkMode();
+        } else {
+           disableDarkMode();
+       }
+    })
