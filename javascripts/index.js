158f9545104d2b994d8e8a7f0f06d26c204b0efa
Michael <mikahado@gmail.com>
2022-10-19 12:24:56 -0400

    updated js, no comments

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,46 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    //GRAB INFO FROM ACTIVITY API
     fetch('http://www.boredapi.com/api/activity/')
     .then(response => response.json())
-    .then(data1 => activity(data1))
+    .then(activityApi => activity(activityApi))
      
-    
-    //GRAB INFO FROM CDC DATABASE
     fetch('/db.json')
     .then(response => response.json())
-    .then(data2 => lifeExpt(data2))
+    .then(cdcData => cdcData.map((stateData)=> {
+        lifeExpt(stateData)  
+    }))
+}) 
     
-    }) 
-    //END OF DOM 
+    let form = document.querySelector('form') 
     
-    
-    //LIFE EXP FROM API NEEDS WORK!!
-    
-    function lifeExpt(entries) {
-
-        let averageMonths = 0
-        let state = ''
-    
-        for (item in entries){
-            console.log(item)
-            Math.ceil(item[0][10] * 12) += averageMonths 
-    
-            item[8] += state
-        }
-            const ul = document.querySelector('#specDatum')
-            const li = document.createElement('li')
-            li.textContent = state + ": " + averageMonths + " months."
-            ul.append(li)
-        }       
-    
-                  
-            
-    
-    //AGE-IN-MONTHS FEATURE
-     let form = document.querySelector('form') 
-    
-     form.addEventListener('submit', (e) => { 
+    form.addEventListener('submit', (e) => { 
          e.preventDefault(); 
          monthCalc(e.target.todayDate.value, e.target.birthDate.value) 
          })
@@ -51,22 +24,17 @@ document.addEventListener("DOMContentLoaded", () => {
         const todayMonth = todayDate.slice(5, 7)
         const birthYear = birthDate.slice(0, 4)
         const birthMonth = birthDate.slice(5,7)
-    
-        let monthsByYear = (todayYear - birthYear) * 12
-        let monthsByYearAdjust = monthsByYear - birthMonth
-        let monthAge = monthsByYearAdjust + parseInt(todayMonth)
-    
-        let percentage = monthAge / 10
+
+        const monthAge = ((todayYear - birthYear) * 12) - (birthMonth) + (parseInt(todayMonth))
+        const percentage = monthAge / 10
     
         let p = document.createElement('p')
         p.textContent = `You have lived  ${monthAge} months. That is ${percentage}% of your 1000 months here on Earth.` 
-        document.querySelector('#myInfo').appendChild(p)
+        document.querySelector('#myInfo').append(p)
         
     }
     
-    //GET IT DONE FEATURE
-    
-    let msg = document.getElementById('comments')
+    const msg = document.getElementById('comments')
     
     msg.addEventListener('submit', (e) => { 
         e.preventDefault(); 
@@ -80,23 +48,36 @@ document.addEventListener("DOMContentLoaded", () => {
         p.textContent = message 
         document.querySelector('#plans').appendChild(p)
      }
-    
-     //SHAKE IT UP FEATURE
-    
-     function activity(act){
 
+     function activity(act){
         const actArray = Object.values(act);
         act = actArray[0]
     
         let p = document.createElement('p')
-        p.textContent = `Dare to do something different. How about this: ${act}!` 
-        document.querySelector('#activity').append(p)
+        p.textContent = "Dare to do something different. How about this: " + act + '!' 
+        document.querySelector('#activity').appendChild(p)
         }
     
-    //CSS MODE CHANGE
+    function lifeExpt(dataLife) {
+
+        
+        // for (item in dataLife){
+        //     const allItems = dataLife[item]
+        //     for (select in allItems){
+        //         const state = allItems[select][8]
+        //         const months = allItems[select][10]*12
+        //         const monthsRounded = Math.ceil(months)
+
+        //         const ul = document.querySelector('#specDatum')
+        //         const li = document.createElement('li')
+        //         li.textContent = `${state}: ${monthsRounded} months`
+        //         ul.append(li)
+        //     }
+        // }     
+    }
     
-    let darkMode = localStorage.getItem('darkMode')
-    let darkModeToggle = document.querySelector('#dark-mode-toggle');
+    const darkMode = localStorage.getItem('darkMode')
+    const darkModeToggle = document.querySelector('#dark-mode-toggle');
     
     const enableDarkMode = () => {
         document.body.classList.add('darkmode')
@@ -109,10 +90,35 @@ document.addEventListener("DOMContentLoaded", () => {
        }
     
     darkModeToggle.addEventListener('click', () => { 
-       darkMode = localStorage.getItem('darkMode') 
+       let darkMode = localStorage.getItem('darkMode') 
        if (darkMode !== "enabled") {
            enableDarkMode();
         } else {
            disableDarkMode();
        }
     })
+
+
+
+
+
+    // Below is the code challege for my project assessment: 
+
+    // const commentForm = document.querySelector("#user-comments-form")
+    // // const commentsSubmit = document.getElementById("user-comments-submit")
+    // const commentText = document.getElementById("user-comments-input")
+    // console.log(commentText)
+
+    // commentForm.addEventListener('submit', (e) => {
+    //     e.preventDefault()
+    //     addComment(commentText.value)
+    // })
+
+    // function addComment(text){
+    //     // text = commentsSubmit.value
+    //     let p = document.createElement("p")
+    //     console.log(p)
+    //     p.textContent = text 
+    //     console.log(p.textContent)
+    //     document.querySelector('#user-comments-div').append(text)
+    //    }
